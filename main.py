4ae1d4112435fbba1a3af17eaa22cafcf8de7293
Nemracair_120 <maria.rodriguezloz03@gmail.com>
2024-11-25 10:46:03 +0100

    Crea el menú básico con mostrar_menu

---
+++ main.py
@@ -0,0 +1,5 @@
+#Para mostrar el menu en el main tambien
+from menu import mostrar_menu
+
+if __name__ == "__main__":
+    mostrar_menu()
---
+++ menu.py
@@ -0,0 +1,10 @@
+# menu.py
+def mostrar_menu():
+    print("1- Sumar")
+    print("2- Restar")
+    print("3- Multiplicar")
+    print("4- Dividir")
+    print("5- Salir")
+    opcion = int(input("Selecciona una opción: "))
+    return opcion
+
